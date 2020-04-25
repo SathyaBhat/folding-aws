44abc2df0a43a398515b66252a112cc6f02cc1dd
Sathyajith Bhat <sathya@sathyasays.com>
2020-04-25 23:47:33 +0300

    mvp: get asg up and running. update readme

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 from aws_cdk import core
 
-from folding_aws.folding_aws_stack import FoldingVpcStack
+from folding_aws.folding_vpc_stack import FoldingVpcStack
+from folding_aws.folding_asg_stack import FoldingAsgStack
 
 
 app = core.App()
-FoldingVpcStack(app, "folding-vpc", env=core.Environment(region="eu-west-1"))
-
+vpc_stack = FoldingVpcStack(app, "folding-vpc", env=core.Environment(region="eu-west-1"))
+asg_stack = FoldingAsgStack(app, "folding-asg", vpc=vpc_stack.vpc, env=core.Environment(region="eu-west-1"))
 app.synth()
