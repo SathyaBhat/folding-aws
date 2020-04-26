520ff8eadfd854907eedb261df4c243ae8bd4a1c
Ninad Pundalik <ni_nad@shortmail.com>
2020-04-26 22:57:32 +0530

    Read some values from the config file instead of hardcoding them

--- app.py
+++ app.py
@@ -4,9 +4,24 @@ from aws_cdk import core
 
 from folding_aws.folding_vpc_stack import FoldingVpcStack
 from folding_aws.folding_asg_stack import FoldingAsgStack
+import sys
+import yaml
 
+try:
+  with open("config.yaml") as f:
+    config = yaml.safe_load(f)
+except yaml.YAMLError as exc:
+  print("Failed parsing the configuration file, exiting", exc)
+  sys.exit(1)
+except OSError as exc:
+  print("Unable to read configuration file, exiting", exc)
+  sys.exit(1)
 
 app = core.App()
-vpc_stack = FoldingVpcStack(app, "folding-vpc", env=core.Environment(region="eu-west-1"))
-asg_stack = FoldingAsgStack(app, "folding-asg", vpc=vpc_stack.vpc, env=core.Environment(region="eu-west-1"))
+vpc_stack = FoldingVpcStack(app, "folding-vpc", env=core.Environment(region=config['aws']['region']))
+asg_stack = FoldingAsgStack(app, "folding-asg", region=config['aws']['region'], vpc=vpc_stack.vpc, 
+                              ami_id=config['aws']['ami_id'], ssh_key=config['aws']['ssh_key'],
+                              max_spot_price=config['aws']['max_spot_price'],
+                              ssh_allow_ip_range=config['aws']['ssh_allow_ip_range'],
+                              env=core.Environment(region=config['aws']['region']))
 app.synth()
--- folding_aws/folding_asg_stack.py
+++ folding_aws/folding_asg_stack.py
@@ -3,35 +3,31 @@ import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_autoscaling as autoscaling
 
 ec2_instance_type = "c5n.large"
-key_name = "sathya_folding_ssh"
-ami = ec2.MachineImage.generic_linux(ami_map = {
-                                            'eu-west-1':'ami-0a8341e7c0a146e7f'
-                                            }
-                                    )
 
 class FoldingAsgStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, id: str, vpc: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, id: str, region: str, vpc: str, ami_id: str, ssh_key: str, max_spot_price: str, ssh_allow_ip_range: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         self.asg = autoscaling.AutoScalingGroup(self,
                                                 "folding-asg",
                                                 instance_type=ec2.InstanceType(ec2_instance_type),
-                                                machine_image = ami,
+                                                machine_image=ec2.MachineImage.generic_linux( ami_map = {region: ami_id}
+                                                ),
                                                 vpc=vpc,
-                                                key_name=key_name,
+                                                key_name=ssh_key,
                                                 vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
                                                 allow_all_outbound=True,
                                                 associate_public_ip_address=True,
                                                 min_capacity=2,
                                                 max_capacity=2,
-                                                spot_price="0.045"
+                                                spot_price=max_spot_price
                                                 )
         sg_ssh_in = ec2.SecurityGroup(self, 
                                     "allow-ssh",
                                     vpc=vpc,
                                     allow_all_outbound=True)
-        sg_ssh_in.add_ingress_rule(ec2.Peer.ipv4("188.26.173.155/32"),
+        sg_ssh_in.add_ingress_rule(ec2.Peer.ipv4(ssh_allow_ip_range),
                                     ec2.Port.tcp(22)
                                 )
 
