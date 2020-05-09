1d471dcc49b7eadf65ebbc4f13609b7ef72d4370
Ninad Pundalik <ninadsp16289+github@gmail.com>
2020-05-09 19:44:53 +0530

    Read AMI ID from packer manifest and update config (#7)
    Co-authored-by: Ninad Pundalik <ni_nad@shortmail.com>

--- packer/install_client.sh
+++ packer/install_client.sh
@@ -17,8 +17,10 @@ sed -i "s/passkey v=''/passkey v='$fah_passkey'/; \
      s/user v=''/user v='$fah_user'/" /tmp/config.xml;
 
 sudo service FAHClient stop;
+sleep 10;
 sudo mv /tmp/config.xml /etc/fahclient/config.xml;
 sudo chown fahclient:root /etc/fahclient/config.xml;
+sleep 10;
 sudo service FAHClient start;
 
 echo "small test to see if the config file has been correctly generated";
